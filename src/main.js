eec46b2efad2de6c31556b26ad03a7eb6a9d1f4c
CeXuY <1398750946@qq.com>
2019-08-01 21:07:30 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 是否开启产品级别提示  不开启   开发阶段  提示越详细越好  开发级别提示
 Vue.config.productionTip = false
 
+// main.js是入口文件
+// 职责1：引入需要依赖的模块（js,包，css,其他模块）
+// 职责2：创建一个 vue 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
